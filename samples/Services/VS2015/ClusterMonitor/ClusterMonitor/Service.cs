297a0e343029f24cca8bc163ec949f76498745ca
vturecek <vturecek@microsoft.com>
2015-05-28 15:06:41 -0700

    Adding Cluster Monitor sample: an example usage of the Service Fabric HTTP management APIs to create a visualization of a cluster and its services.

---
+++ samples/Services/VS2015/ClusterMonitor/ClusterMonitor/Service.cs
@@ -0,0 +1,23 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace ClusterMonitor
+{
+    using System.Fabric.Description;
+    using Microsoft.ServiceFabric.Services;
+
+    public class Service : StatelessService
+    {
+        public const string ServiceTypeName = "ClusterMonitorType";
+
+        protected override ICommunicationListener CreateCommunicationListener()
+        {
+            ConfigurationSettings configSettings =
+                this.ServiceInitializationParameters.CodePackageActivationContext.GetConfigurationPackageObject("Config").Settings;
+
+            return new OwinCommunicationListener("cluster", new Startup(configSettings));
+        }
+    }
+}
